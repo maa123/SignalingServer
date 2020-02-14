87a1e21e6544a933f99f36abac333a6551218fd3
maa123 <maa123.lq@gmail.com>
2020-02-14 09:21:36 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 3197 });
+
+
+
+wss.on('connection', function connection(ws) {
+	ws.roomId = "";
+	ws.on('message', function incoming(message) {
+		console.log('received: %s', message);
+		try{
+			const data = JSON.parse(message);
+			if(data.type === "connect"){
+				ws.roomId = data.Id;
+				ws.send(JSON.stringify({'type': 'meta', 'msg': 'connected.'}));
+			}else if(data.type === "signaling"){
+				const targ = wss.clients.find(client => {
+					if(client !== ws && client.readyState === WebSocket.OPEN && ws.roomId === client.roomId){
+						return true;
+					}return false;
+				});
+				targ.send(JSON.stringify({'type': 'signal', 'data': data.SDP}));
+			}
+		}catch(e){
+			console.warn(e);
+		}
+	});
+	//ws.send('something');
+});
